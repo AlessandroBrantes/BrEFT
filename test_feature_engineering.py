d1be7fac8a5ca38f534f127a881dcaf74f59bfc4
AlessandroBrantes <128158510+AlessandroBrantes@users.noreply.github.com>
2023-04-17 15:43:17 -0300

    Update test_feature_engineering.py

--- test_feature_engineering.py
+++ test_feature_engineering.py
@@ -6,31 +6,34 @@ import pandas as pd
 if not mt5.initialize():
     print("initialize() failed")
     mt5.shutdown()
-
-# Obter dados históricos
-symbol = "GOLD"  # ou "XAUUSD", se a sua corretora usar essa nomenclatura
-rates = mt5.copy_rates_from_pos(symbol, mt5.TIMEFRAME_M1, 0, 1000)
-
-# Desconectar do terminal
-mt5.shutdown()
-
-# Converter os dados históricos para um DataFrame do pandas
-data = pd.DataFrame(rates)
-
-if data.empty:
-    print("O DataFrame está vazio.")
 else:
-    print("O DataFrame contém dados.")
+    print("initialize() succeeded")
+    print("Terminal info:", mt5.terminal_info())
+    print("Account info:", mt5.account_info())
 
-    # Renomear as colunas
-    data.columns = ['time', 'open', 'high', 'low', 'close', 'tick_volume', 'spread', 'real_volume']
+    # Obter dados históricos
+    symbol = "GOLD"  # ou "XAUUSD", se a sua corretora usar essa nomenclatura
+    rates = mt5.copy_rates_from_pos(symbol, mt5.TIMEFRAME_M1, 0, 1000)
 
-    # Imprimir colunas do DataFrame
-    print("DataFrame columns:", data.columns)
+    # Desconectar do terminal
+    mt5.shutdown()
 
-    # Imprimir as primeiras linhas do DataFrame
-    print(data.head())
+    # Converter os dados históricos para um DataFrame do pandas
+    data = pd.DataFrame(rates)
 
-    # Criar features
-    data = create_features(data, open_col="open", high_col="high", low_col="low", close_col="close", volume_col="tick_volume")
+    if data.empty:
+        print("O DataFrame está vazio.")
+    else:
+        print("O DataFrame contém dados.")
 
+        # Renomear as colunas
+        data.columns = ['time', 'open', 'high', 'low', 'close', 'tick_volume', 'spread', 'real_volume']
+
+        # Imprimir colunas do DataFrame
+        print("DataFrame columns:", data.columns)
+
+        # Imprimir as primeiras linhas do DataFrame
+        print(data.head())
+
+        # Criar features
+        data = create_features(data, open_col="open", high_col="high", low_col="low", close_col="close", volume_col="tick_volume")
