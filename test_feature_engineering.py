25df41f5f341e8f1a654c83d220122f9453dde6a
AlessandroBrantes <128158510+AlessandroBrantes@users.noreply.github.com>
2023-04-17 11:08:22 -0300

    Update test_feature_engineering.py

--- test_feature_engineering.py
+++ test_feature_engineering.py
@@ -1,18 +1,25 @@
-import pandas as pd
-from feature_engineering import create_features
 import MetaTrader5 as mt5
+from feature_engineering import create_features
+import pandas as pd
 
+# Conectar ao terminal MetaTrader 5
 if not mt5.initialize():
     print("initialize() failed")
     mt5.shutdown()
 
-symbol = "GOLD"
+# Obter dados históricos
+symbol = "GOLD"  # ou "XAUUSD", se a sua corretora usar essa nomenclatura
 rates = mt5.copy_rates_from_pos(symbol, mt5.TIMEFRAME_M1, 0, 1000)
 
+# Desconectar do terminal
 mt5.shutdown()
 
+# Converter os dados históricos para um DataFrame do pandas
 data = pd.DataFrame(rates)
+
+# Imprimir colunas do DataFrame
+print("DataFrame columns:", data.columns)
+
+# Criar features
 data = create_features(data)
 
-print("Features criadas com sucesso:")
-print(data.head())
