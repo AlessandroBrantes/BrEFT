063702489874f95cf418cf6f9705324e3830095a
AlessandroBrantes <128158510+AlessandroBrantes@users.noreply.github.com>
2023-04-17 11:32:58 -0300

    Update feature_engineering.py

--- feature_engineering.py
+++ feature_engineering.py
@@ -4,3 +4,4 @@ def create_features(data, open_col, high_col, low_col, close_col, volume_col):
     data = add_all_ta_features(data, open=open_col, high=high_col, low=low_col, close=close_col, volume=volume_col)
     return data
 
+
