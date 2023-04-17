4c6e5bced593b8699e089751a80eb12b62f0f15a
AlessandroBrantes <128158510+AlessandroBrantes@users.noreply.github.com>
2023-04-17 11:31:54 -0300

    Update BrEFT_GOLD.py
    O código BrEFT_GOLD.py parece estar correto para se conectar ao terminal MetaTrader 5 e obter dados históricos. No entanto, não faz nada com os dados após desconectar do terminal.
    
    Se você deseja aplicar as mesmas transformações de engenharia de recursos que fizemos no arquivo test_feature_engineering.py, você pode atualizar o arquivo BrEFT_GOLD.py da seguinte maneira:  
    
    Isso irá aplicar as transformações de engenharia de recursos usando o módulo feature_engineering e salvar os dados resultantes em um arquivo CSV chamado GOLD_features.csv.

--- BrEFT_GOLD.py
+++ BrEFT_GOLD.py
@@ -1,4 +1,6 @@
 import MetaTrader5 as mt5
+from feature_engineering import create_features
+import pandas as pd
 
 # Conectar ao terminal MetaTrader 5
 if not mt5.initialize():
@@ -11,3 +13,13 @@ rates = mt5.copy_rates_from_pos(symbol, mt5.TIMEFRAME_M1, 0, 1000)
 
 # Desconectar do terminal
 mt5.shutdown()
+
+# Converter os dados históricos para um DataFrame do pandas
+data = pd.DataFrame(rates)
+
+# Criar features
+data = create_features(data, open_col="open", high_col="high", low_col="low", close_col="close", volume_col="tick_volume")
+
+# Faça algo com os dados, por exemplo, salvar em um arquivo CSV
+data.to_csv("GOLD_features.csv", index=False)
+
