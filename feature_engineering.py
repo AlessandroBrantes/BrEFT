c015bdb40546cf61f15eba2a027c62b61f1cec0f
AlessandroBrantes <128158510+AlessandroBrantes@users.noreply.github.com>
2023-04-17 11:28:25 -0300

    Update feature_engineering.py
    E também modifique o arquivo feature_engineering.py para aceitar os novos argumentos:

--- feature_engineering.py
+++ feature_engineering.py
@@ -1,7 +1,6 @@
-import pandas as pd
 from ta import add_all_ta_features
 
-def create_features(data):
-    # Calcular indicadores técnicos
-    data = add_all_ta_features(data, open="open", high="high", low="low", close="close", volume="tick_volume")
+def create_features(data, open_col, high_col, low_col, close_col, volume_col):
+    data = add_all_ta_features(data, open=open_col, high=high_col, low=low_col, close=close_col, volume=volume_col)
     return data
+
