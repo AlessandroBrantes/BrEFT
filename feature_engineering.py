9c9effb409103b29cc03b7f154300cc2ac5fdf72
AlessandroBrantes <128158510+AlessandroBrantes@users.noreply.github.com>
2023-04-17 22:22:02 -0300

    Update feature_engineering.py
    Obrigado por compartilhar a saída do script. O erro ocorre porque a coluna 'target' não existe no DataFrame data_with_features. Eu percebi que esqueci de adicionar a etapa de criação da coluna 'target' no script.
    
    Por favor, adicione a seguinte função no arquivo feature_engineering.py:
    
    python
    Copy code
    def create_target(data, close_col='close', shift=-1):
        data['target'] = data[close_col].shift(shift)
        data.dropna(inplace=True)

--- feature_engineering.py
+++ feature_engineering.py
@@ -1,5 +1,25 @@
+import pandas as pd
+import numpy as np
+import talib
 from ta import add_all_ta_features
 
+def create_target(data, close_col='close', shift=-1):
+    data['target'] = data[close_col].shift(shift)
+    data.dropna(inplace=True)
+
 def create_features(data, open_col, high_col, low_col, close_col, volume_col):
-    data = add_all_ta_features(data, open=open_col, high=high_col, low=low_col, close=close_col, volume=volume_col)
+    data = add_all_ta_features(
+        data,
+        open=open_col,
+        high=high_col,
+        low=low_col,
+        close=close_col,
+        volume=volume_col,
+        fillna=True
+    )
+    
+    # Adicionar features personalizadas aqui, se necessário
+    # Exemplo: data['custom_feature'] = ...
+
     return data
+
