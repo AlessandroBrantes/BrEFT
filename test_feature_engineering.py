08e793eb70254deda1a7b1894b1af684e56a4dbb
AlessandroBrantes <128158510+AlessandroBrantes@users.noreply.github.com>
2023-04-17 22:24:45 -0300

    Update test_feature_engineering.py
    E aqui está o arquivo test_feature_engineering.py atualizado para incluir a chamada à função create_target:

--- test_feature_engineering.py
+++ test_feature_engineering.py
@@ -1,46 +1,32 @@
 import MetaTrader5 as mt5
-import pandas as pd
 from datetime import datetime
-from feature_engineering import create_features
+import pandas as pd
 from sklearn.model_selection import train_test_split
+from feature_engineering import create_features, create_target
 
-def get_historical_data(symbol, timeframe, start_time, end_time):
-    if not mt5.symbol_select(symbol, True):
-        print(f"Não foi possível selecionar {symbol} no MetaTrader 5.")
-        return pd.DataFrame()
-    
-    rates = mt5.copy_rates_range(symbol, timeframe, start_time, end_time)
-    
-    if rates is None or len(rates) == 0:
-        print(f"Não foi possível obter dados históricos para {symbol}.")
-        return pd.DataFrame()
-    
-    data = pd.DataFrame(rates)
-    data['time'] = pd.to_datetime(data['time'], unit='s')
-    data.set_index('time', inplace=True)
-    data = data[['open', 'high', 'low', 'close', 'tick_volume', 'spread', 'real_volume']]
-    
-    return data
+symbol = "PETR4"
+timezone_offset = -3
+timeframe = mt5.TIMEFRAME_M1
+start_time = datetime(2023, 4, 3)
+end_time = datetime(2023, 4, 4)
 
 if not mt5.initialize():
     print("initialize() failed")
     mt5.shutdown()
+    quit()
 
-print("initialize() succeeded")
-print(mt5.terminal_info())
-print(mt5.account_info())
-
-symbol = "GOLD"
-timeframe = mt5.TIMEFRAME_M1
-start_time = datetime(2023, 4, 1)
-end_time = datetime(2023, 4, 15)
-
-data = get_historical_data(symbol, timeframe, start_time, end_time)
+rates = mt5.copy_rates_range(symbol, timeframe, start_time, end_time)
+data = pd.DataFrame(rates)
+data['time'] = pd.to_datetime(data['time'], unit='s')
+data.set_index('time', inplace=True)
 
 if data.empty:
     print("O DataFrame está vazio.")
 else:
     data_with_features = create_features(data, open_col="open", high_col="high", low_col="low", close_col="close", volume_col="tick_volume")
+    
+    # Adicionar a coluna 'target'
+    create_target(data_with_features, close_col='close', shift=-1)
     
     # Imprimir as primeiras linhas dos dados com as features técnicas adicionadas
     print(data_with_features.head())
@@ -61,5 +47,6 @@ else:
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 mt5.shutdown()
+
 
 
