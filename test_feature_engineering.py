4df3447505c54ab7b9f82772aa0056587d8b5265
AlessandroBrantes <128158510+AlessandroBrantes@users.noreply.github.com>
2023-04-17 22:18:23 -0300

    Update test_feature_engineering.py
    O erro aconteceu porque não há dados disponíveis no DataFrame após a criação das features técnicas. Isso pode ser devido à falta de dados históricos, problemas na conexão com o MetaTrader 5 ou à exclusão de colunas com muitos valores NaN durante a criação das features.
    
    Para investigar e resolver o problema, vamos adicionar algumas mensagens de depuração ao script "test_feature_engineering.py". Adicione as seguintes linhas antes da linha 57:

--- test_feature_engineering.py
+++ test_feature_engineering.py
@@ -1,11 +1,8 @@
 import MetaTrader5 as mt5
 import pandas as pd
 from datetime import datetime
-import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn.tree import DecisionTreeClassifier
-
 from feature_engineering import create_features
+from sklearn.model_selection import train_test_split
 
 def get_historical_data(symbol, timeframe, start_time, end_time):
     if not mt5.symbol_select(symbol, True):
@@ -43,26 +40,26 @@ data = get_historical_data(symbol, timeframe, start_time, end_time)
 if data.empty:
     print("O DataFrame está vazio.")
 else:
-    data = create_features(data, open_col="open", high_col="high", low_col="low", close_col="close", volume_col="tick_volume")
-    data['target_column'] = np.sign(data['close'].pct_change().shift(-1))
+    data_with_features = create_features(data, open_col="open", high_col="high", low_col="low", close_col="close", volume_col="tick_volume")
     
-    # Remover linhas com valores NaN
-    data.dropna(inplace=True)
-    
-    # Preparar os dados de entrada (features) e saída (target)
-    X = data.drop(columns=["target_column"])
-    y = data["target_column"]
+    # Imprimir as primeiras linhas dos dados com as features técnicas adicionadas
+    print(data_with_features.head())
 
     # Dividir os dados em conjuntos de treinamento e teste
+    target_variable = "target"
+    X = data_with_features.drop(columns=[target_variable])
+    y = data_with_features[target_variable]
+
+    # Adicionar mensagens de depuração
+    print("Shape of X:", X.shape)
+    print("Shape of y:", y.shape)
+    print("Head of X:")
+    print(X.head())
+    print("Head of y:")
+    print(y.head())
+
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-    # Treinar o modelo
-    clf = DecisionTreeClassifier()
-    clf.fit(X_train, y_train)
-
-    # Avaliar o desempenho do modelo
-    score = clf.score(X_test, y_test)
-    print("Accuracy:", score)
-
 mt5.shutdown()
+
 
