66737374ebae5cb6a21780b4af51e75c04ab2438
AlessandroBrantes <128158510+AlessandroBrantes@users.noreply.github.com>
2023-04-17 22:44:50 -0300

    Update feature_engineering.py

--- feature_engineering.py
+++ feature_engineering.py
@@ -1,27 +1,23 @@
 import pandas as pd
 import numpy as np
 from ta import add_all_ta_features
-import warnings
 
-warnings.filterwarnings("ignore", category=RuntimeWarning)
+def create_target(data, close_col='close', shift=-1):
+    data['target'] = data[close_col].shift(shift)
+    data.dropna(inplace=True)
 
-def create_features(df):
-    df['time'] = pd.to_datetime(df['time'], format="%Y.%m.%d %H:%M:%S")
-    df.set_index('time', inplace=True)
-    
-    df = add_all_ta_features(
-        df, open="open", high="high", low="low", close="close", volume="tick_volume"
+def create_features(data, open_col, high_col, low_col, close_col, volume_col):
+    data = add_all_ta_features(
+        data,
+        open=open_col,
+        high=high_col,
+        low=low_col,
+        close=close_col,
+        volume=volume_col,
+        fillna=True
     )
+    
+    # Adicionar features personalizadas aqui, se necessário
+    # Exemplo: data['custom_feature'] = ...
 
-    return df
-
-def create_target(df, shift=-1):
-    df['target'] = df['close'].shift(shift)
-    df.dropna(inplace=True)
-    return df
-
-if __name__ == "__main__":
-    data = pd.read_csv("GOLD_M1.csv")
-    data = create_features(data)
-    data = create_target(data)
-    data.to_csv("GOLD_M1_processed.csv")
+    return data
