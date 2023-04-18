9b1be1a7bd74f4c226b24420c79789e56f688ce6
AlessandroBrantes <128158510+AlessandroBrantes@users.noreply.github.com>
2023-04-17 22:34:42 -0300

    Update test_feature_engineering.py
    Para executar esses scripts, salve-os como feature_engineering.py e test_feature_engineering.py, respectivamente. Primeiro, execute feature_engineering.py e, em seguida, execute test_feature_engineering.py para testar o funcionamento do código e ver o resultado.

--- test_feature_engineering.py
+++ test_feature_engineering.py
@@ -1,52 +1,24 @@
-import MetaTrader5 as mt5
-from datetime import datetime
 import pandas as pd
-from sklearn.model_selection import train_test_split
 from feature_engineering import create_features, create_target
 
-symbol = "GOLD"
-timezone_offset = -3
-timeframe = mt5.TIMEFRAME_M1
-start_time = datetime(2023, 4, 3)
-end_time = datetime(2023, 4, 4)
-
-if not mt5.initialize():
-    print("initialize() failed")
-    mt5.shutdown()
-    quit()
-
-rates = mt5.copy_rates_range(symbol, timeframe, start_time, end_time)
-data = pd.DataFrame(rates)
-data['time'] = pd.to_datetime(data['time'], unit='s')
-data.set_index('time', inplace=True)
-
-if data.empty:
-    print("O DataFrame está vazio.")
-else:
-    data_with_features = create_features(data, open_col="open", high_col="high", low_col="low", close_col="close", volume_col="tick_volume")
+def test_feature_engineering():
+    data = pd.read_csv("GOLD_M1.csv")
+    data = create_features(data)
+    data = create_target(data)
     
-    # Adicionar a coluna 'target'
-    create_target(data_with_features, close_col='close', shift=-1)
+    print(data.head())
     
-    # Imprimir as primeiras linhas dos dados com as features técnicas adicionadas
-    print(data_with_features.head())
+    X = data.drop(columns=["target"])
+    y = data["target"]
 
-    # Dividir os dados em conjuntos de treinamento e teste
-    target_variable = "target"
-    X = data_with_features.drop(columns=[target_variable])
-    y = data_with_features[target_variable]
-
-    # Adicionar mensagens de depuração
     print("Shape of X:", X.shape)
     print("Shape of y:", y.shape)
-    print("Head of X:")
-    print(X.head())
-    print("Head of y:")
-    print(y.head())
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    print("Head of X:\n", X.head())
+    print("Head of y:\n", y.head())
 
-mt5.shutdown()
+if __name__ == "__main__":
+    test_feature_engineering()
 
 
 
